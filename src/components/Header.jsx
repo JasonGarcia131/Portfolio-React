e595b2a5111f562677ad07a9a8488bfdae402dec
Jason Garcia <jaygarcia0530@gmail.com>
2022-12-17 16:47:56 -0800

    added react icon envelope and phone

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,15 @@
 import { Link } from "react-router-dom"
-
+import {FaEnvelope} from "react-icons/fa"
+import {FaPhoneAlt} from "react-icons/fa"
 
 function Header() {
 
     const BASEURL = "https://jasongarcia131.github.io/Portfolio-React/#/"
     const pathname = window.location.href
-        
-    const home = pathname === `${BASEURL}` ? <p style={{opacity: ".5"}}><Link to="/">Home</Link></p> : <p><Link to="/">Home</Link></p>
-    const aboutMe = pathname === `${BASEURL}AboutMe` ? <p style={{ opacity: ".5"}}><Link to="/AboutMe">About Me</Link></p> : <p><Link to="/AboutMe">About Me</Link></p>
-    const resume = pathname === `${BASEURL}Resume` ? <p style={{ opacity: ".5"}}><Link to="/Resume">Resume</Link></p> : <p><Link to="/Resume">Resume</Link></p>
+
+    const home = pathname === `${BASEURL}` ? <p style={{ opacity: ".5" }}><Link to="/">Home</Link></p> : <p><Link to="/">Home</Link></p>
+    const aboutMe = pathname === `${BASEURL}AboutMe` ? <p style={{ opacity: ".5" }}><Link to="/AboutMe">About Me</Link></p> : <p><Link to="/AboutMe">About Me</Link></p>
+    const resume = pathname === `${BASEURL}Resume` ? <p style={{ opacity: ".5" }}><Link to="/Resume">Resume</Link></p> : <p><Link to="/Resume">Resume</Link></p>
 
     return (
         <header>
@@ -16,24 +17,23 @@ function Header() {
                 <p>
                     Contact Me:
                 </p>
-                <p>
+                <div>
+                    <FaPhoneAlt/>
                     323-316-8978
-                </p>
-                <p>
+                </div>
+                <div>
+                    <FaEnvelope/>
                     <a href="mailto:jaygarcia0530@gmail.com"> jaygarcia0530@gmail.com </a>
-                </p>
+                </div>
             </div>
             <div className="header-links">
-                <p>
-                    Links:
-                </p>
+                {home}
                 {resume}
                 {aboutMe}
                 <p>
                     <a href="https://github.com/JasonGarcia131">GitHub</a>
                 </p>
-                
-                {home}
+
             </div>
         </header>
     )
